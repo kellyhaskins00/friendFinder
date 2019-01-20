cf97b6ee92d5c2a1f22a6a836ba3084ad11ded3c
Kelly Haskins <kellyhaskins00@gmail.com>
2019-01-19 22:31:36 -0800

    updated

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friendMatch = [
+    {
+      name: "Chomper",
+      photo: "https://vignette.wikia.nocookie.net/donbluth/images/1/10/09.jpg/revision/latest?cb=20170808183356",
+      scores: ["2", "3", "2", "2", "2","1", "2", "2", "2", "2"]
+    }, {
+      name: "Spike",
+      photo: "https://vignette.wikia.nocookie.net/parody/images/e/e7/Spike_land_before_time.jpg/revision/latest?cb=20150830042057",
+      scores: ["3", "3", "3", "2", "3","2", "3", "3", "3", "2"]
+    }, {
+      name: "Little Foot",
+      photo: "https://vignette.wikia.nocookie.net/landbeforetime/images/f/fe/LBT_HeroImage_v1.png/revision/latest?cb=20180422005007",
+      scores: ["1", "3", "1", "1", "1","1", "1", "3", "1", "3"]
+    }, {
+      name: "Ducky",
+      photo: "https://vignette.wikia.nocookie.net/landbeforetime/images/8/8d/Ducky_infobox.png/revision/latest?cb=20180422005001",
+      scores: ["3", "1", "1", "2", "1","2", "3", "3", "3", "1"]
+    },
+  
+  ];
+  
+  module.exports = friendMatch;
